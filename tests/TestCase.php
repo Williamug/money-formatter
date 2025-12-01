5d8d5547096c2759ea4a6a66c904a9137ad940ba
William Asaba <asabawilliamdk@yahoo.com>
2025-12-01 15:39:30 +0300

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,31 +8,31 @@ use Williamug\MoneyFormatter\BladeServiceProvider;
 
 class TestCase extends Orchestra
 {
-    public static $latestResponse;
+  public static $latestResponse;
 
-    protected function setUp(): void
-    {
-        parent::setUp();
+  protected function setUp(): void
+  {
+    parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Williamug\\MoneyFormatter\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
-        );
-    }
+    Factory::guessFactoryNamesUsing(
+      fn(string $modelName) => 'Williamug\\MoneyFormatter\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+    );
+  }
 
-    protected function getPackageProviders($app)
-    {
-        return [
-          BladeServiceProvider::class,
-        ];
-    }
+  protected function getPackageProviders($app)
+  {
+    return [
+      BladeServiceProvider::class,
+    ];
+  }
 
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
+  public function getEnvironmentSetUp($app)
+  {
+    config()->set('database.default', 'testing');
 
-        /*
+    /*
             $migration = include __DIR__.'/../database/migrations/create_money-formatter_table.php.stub';
             $migration->up();
             */
-    }
+  }
 }
